ae1b1d206da0b8288ffa242f2227ce5cbc7c48c3
KIMDH <kimdh@goodmit.co.kr>
2022-04-14 10:01:02 +0900

    cicd test

--- app/app.js
+++ app/app.js
@@ -1,1 +1,13 @@
-console.log("Ano");
+var express = require('express')
+
+var app = express()
+
+app.use(express.static('static'))
+
+app.listen(5000, function () {
+    console.log("app.js Started!")
+})
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/public/main.html")
+})
